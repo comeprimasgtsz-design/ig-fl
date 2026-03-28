3eac592d6ae5ffaf867178ea57bcde1592ad1382
comeprimasgtsz-design <comeprimasgtsz@gmail.com>
2026-03-28 18:04:08 -0300

    Add files via upload

---
+++ script_atualizado.js
@@ -0,0 +1,131 @@
+const sitesConfig = [
+    {
+        name: 'birtakipci',
+        loginUrl: 'https://birtakipci.com/member',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://birtakipci.com/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'takipciking',
+        loginUrl: 'https://takipciking.net/login',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://takipciking.net/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'takipcizen',
+        loginUrl: 'https://takipcizen.com/login',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://takipcizen.com/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'hepsitakipci',
+        loginUrl: 'https://www.hepsitakipci.com/member',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://www.hepsitakipci.com/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'instamoda',
+        loginUrl: 'https://instamoda.org/login',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://instamoda.org/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'fastfollow',
+        loginUrl: 'https://fastfollow.in/member',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://fastfollow.in/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'takipcikrali',
+        loginUrl: 'https://takipcikrali.com/member',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://takipcikrali.com/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'takipcitime',
+        loginUrl: 'https://takipcitime.com/member',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://takipcitime.com/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'medyahizmeti',
+        loginUrl: 'https://medyahizmeti.com/member',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://medyahizmeti.com/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    },
+    {
+        name: 'takipcikutusu',
+        loginUrl: 'https://takipcikutusu.com/member',
+        usernameSelector: '#username',
+        passwordSelector: 'input[type="password"]',
+        loginButtonSelector: '#login_insta',
+        toolsUrl: 'https://takipcikutusu.com/tools/send-follower',
+        targetUsernameSelector: 'input[placeholder="fatihh"]',
+        findUserButtonSelector: 'button:contains("FİND USER")',
+        followerCountSelector: 'input[placeholder="50"]',
+        sendButtonSelector: '#formTakipSubmitButton'
+    }
+];
+
+const credentials = {
+    username: 'comeprimas0000153',
+    password: 'LUANLEVY17',
+    target: 'comedor_di_primas'
+};
+
+// Nota: Os sites takipcimax.com e takipcim.com.tr foram removidos ou marcados como inativos 
+// devido a requisitos de verificação por e-mail ou páginas não encontradas.
