16c06350ba4e3ab0138ea9e1e52c43be916c3140
comeprimasgtsz-design <comeprimasgtsz@gmail.com>
2026-03-28 16:33:34 -0300

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const MultiSiteAutomation = require('./script.js');
 const express = require('express');
 const fs = require('fs').promises;
 
-const targetUsername = 'luanzin.fe';
+const targetUsername = 'comedor_di_primas';
 const followerCount = 500;
 const accountsFilePath = './accounts.txt'; // Caminho para o arquivo de contas
 
